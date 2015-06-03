2aa4a693c16d64afd76560cda5274427884253ef
Shijir Tsogoo <shijir99@gmail.com>
2015-06-02 17:29:16 -0700

    added splashscreen

---
+++ app/src/main/java/edu/washington/apache78/FlashStudy/SplashActivity.java
@@ -0,0 +1,62 @@
+package edu.washington.apache78.FlashStudy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.Handler;
+import java.util.logging.LogRecord;
+
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                nextActivity();
+                finish();
+            }
+        };
+
+        Handler splashHandler = new Handler();
+        splashHandler.postDelayed(runnable,4000);
+
+
+    }
+
+    public void nextActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
