e2da54e6326ec401ec618c8bb370fd84e150d08d
Shijir Tsogoo <shijir99@gmail.com>
2015-06-07 06:11:09 -0700

    retrieved content of txt type

--- app/src/main/java/edu/washington/apache78/FlashStudy/activities/RetrieveContentsWithProgressDialogActivity.java
+++ app/src/main/java/edu/washington/apache78/FlashStudy/activities/RetrieveContentsWithProgressDialogActivity.java
@@ -1,5 +1,6 @@
 package edu.washington.apache78.FlashStudy.activities;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -19,7 +20,17 @@ import com.google.android.gms.drive.DriveFile;
 import com.google.android.gms.drive.DriveFile.DownloadProgressListener;
 import com.google.android.gms.drive.DriveId;
 import com.google.android.gms.drive.OpenFileActivityBuilder;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi.DriveContentsResult;
+import com.google.android.gms.drive.DriveApi.DriveIdResult;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFile;
+import com.google.android.gms.drive.DriveId;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import edu.washington.apache78.FlashStudy.R;
 
@@ -56,20 +67,85 @@ public class RetrieveContentsWithProgressDialogActivity extends BaseDriveActivit
 
         // If there is a selected file, open its contents.
         if (mSelectedFileDriveId != null) {
-            showMessage("File ID: "+mSelectedFileDriveId.encodeToString());
+
+            //showMessage("File ID: "+mSelectedFileDriveId.encodeToString());
+
+            Drive.DriveApi.fetchDriveId(getGoogleApiClient(), mSelectedFileDriveId.getResourceId()).setResultCallback(idCallback);
+
             return;
+
         }
 
         // Let the user pick an mp4 or a jpeg file if there are
         // no files selected by the user.
         IntentSender intentSender = Drive.DriveApi
                 .newOpenFileActivityBuilder()
-                .setMimeType(new String[]{ "video/mp4", "image/jpeg" })
+                .setMimeType(new String[]{ "text/plain", "application/vnd.google-apps.document" })
                 .build(getGoogleApiClient());
         try {
             startIntentSenderForResult(intentSender, REQUEST_CODE_OPENER, null, 0, 0, 0);
         } catch (SendIntentException e) {
             Log.w(TAG, "Unable to send intent", e);
+        }
+    }
+    final private ResultCallback<DriveIdResult> idCallback = new ResultCallback<DriveIdResult>() {
+        @Override
+        public void onResult(DriveIdResult result) {
+            new RetrieveDriveFileContentsAsyncTask(
+                    RetrieveContentsWithProgressDialogActivity.this).execute(result.getDriveId());
+        }
+    };
+
+    final private class RetrieveDriveFileContentsAsyncTask
+            extends ApiClientAsyncTask<DriveId, Boolean, String> {
+
+        public RetrieveDriveFileContentsAsyncTask(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected String doInBackgroundConnected(DriveId... params) {
+            String contents = null;
+            DriveFile file = Drive.DriveApi.getFile(getGoogleApiClient(), params[0]);
+            DriveContentsResult driveContentsResult =
+                    file.open(getGoogleApiClient(), DriveFile.MODE_READ_ONLY, null).await();
+            if (!driveContentsResult.getStatus().isSuccess()) {
+                return null;
+            }
+            DriveContents driveContents = driveContentsResult.getDriveContents();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(driveContents.getInputStream()));
+            StringBuilder builder = new StringBuilder();
+            String line;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+                contents = builder.toString();
+            } catch (IOException e) {
+                Log.e(TAG, "IOException while reading from the stream", e);
+            }
+
+            driveContents.discard(getGoogleApiClient());
+            return contents;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            Intent driveAuthenticate = new Intent(RetrieveContentsWithProgressDialogActivity.this, MainActivity.class);
+
+            if (result == null) {
+                showMessage("Error while reading from the file");
+
+                startActivity(driveAuthenticate);
+
+                return;
+            }
+            showMessage("File contents: " + result);
+
+
         }
     }
 
@@ -78,6 +154,7 @@ public class RetrieveContentsWithProgressDialogActivity extends BaseDriveActivit
         if (requestCode == REQUEST_CODE_OPENER && resultCode == RESULT_OK) {
             mSelectedFileDriveId = (DriveId) data.getParcelableExtra(
                     OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);
+
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
