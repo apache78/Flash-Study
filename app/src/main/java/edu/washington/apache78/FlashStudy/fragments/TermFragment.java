bd74dfe428d827bc6775f48a1bc3d175e3bc9a37
apache78 <apache78@uw.edu>
2015-06-08 22:11:07 -0700

    getting fragments to worK

--- app/src/main/java/edu/washington/apache78/FlashStudy/fragments/TermFragment.java
+++ app/src/main/java/edu/washington/apache78/FlashStudy/fragments/TermFragment.java
@@ -1,18 +1,18 @@
 package edu.washington.apache78.FlashStudy.fragments;
 
-import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
-import android.view.*;
-import android.widget.*;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import edu.washington.apache78.FlashStudy.R;
 import edu.washington.apache78.FlashStudy.includes.NoteProgress;
 
 
 public class TermFragment extends Fragment {
-
+	boolean last;
 	public TermFragment() {
 		// Required empty public constructor
 	}
@@ -22,7 +22,12 @@ public class TermFragment extends Fragment {
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
 		View v = inflater.inflate(R.layout.fragment_term, container, false);
+		last = false;
 
+		if(last){
+			v.findViewById(R.id.btnDefNextTerm).setVisibility(View.GONE);
+			v.findViewById(R.id.btnDone).setVisibility(View.VISIBLE);
+		}
 		NoteProgress progress = (NoteProgress)getArguments().getSerializable("progress");
         ((TextView)v.findViewById(R.id.termText)).setText(progress.getCurrentCard().term);
 
